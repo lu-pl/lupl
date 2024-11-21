e2532b230ee801c3b6b08962d99344834a5bb405
Lukas Plank <lupl@tuta.io>
2024-11-21 18:56:21 +0100

    feat: implement mutual dependency mixin for pydantic models
    Closes #12.

--- upto/__init__.py
+++ upto/__init__.py
@@ -1,3 +1,4 @@
 from upto.compose_router import ComposeRouter
 from upto.pydantic_tools.curry_model import CurryModel
 from upto.pydantic_tools.model_constructors import init_model_from_kwargs
+from upto.pydantic_tools.mutual_constraint_validator import _MutualConstraintMixin
